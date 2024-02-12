13ffa2e4131a688f8089e5befa767f7230bc9150
Alex Yuan <alex@alexyuan.me>
2024-02-11 19:05:04 -0500

    new repo

---
+++ server.js
@@ -0,0 +1,9 @@
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.use("*", serveStatic({ root: "dist" }));
+
+serve(app);
