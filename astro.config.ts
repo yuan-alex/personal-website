8c82cd8bdb5fdca7ef1186fb17c6d5fee69bb8b8
Alex Yuan <alex@alexyuan.me>
2024-02-19 23:30:03 -0500

    fix port

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [tailwind()],
@@ -8,4 +8,7 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
+  server: {
+    port: 3000,
+  },
 });
