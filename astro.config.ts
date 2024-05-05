f04128d849de299c2bff2438ace31256311efce3
Alex Yuan <alex@alexyuan.me>
2024-05-04 22:40:49 -0400

    move to static

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,8 @@
-import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
   server: {
     port: 3000,
   },
